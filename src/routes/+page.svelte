594dd8351463950ee1a3708e5c9d56f29459d8c6
Suyooo <me@suyo.be>
2022-10-14 18:34:09 +0200

    Add Tailwind CSS

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import Guess from "./Guess.svelte";
+
+    export const prerender = true;
+
+    const guesses: { guess: string }[] = [
+        {guess: "test"},
+        {guess: "test"},
+        {guess: "test"}
+    ]
+
+    function change(i) {
+        guesses[i].guess = prompt("do it");
+    }
+</script>
+
+<header>
+    <h1>Welcome to ASASSASA</h1>
+</header>
+
+<main>
+    {#each guesses as g}
+        <Guess guess={g}/>
+    {/each}
+
+    <button on:click={() => change(0)}>Change 0</button>
+    <button on:click={() => change(1)}>Change 1</button>
+    <button on:click={() => change(2)}>Change 2</button>
+</main>
---
+++ src/routes/Guess.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    export let guess;
+    import "../app.css";
+</script>
+
+<div class="w-full">I am {guess.guess}!</div>
