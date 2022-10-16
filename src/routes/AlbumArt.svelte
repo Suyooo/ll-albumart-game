79e423e37418ac7c0d7de225483e41a7853683a2
Suyooo <me@suyo.be>
2022-10-16 13:09:39 +0200

    Fix "Loading..." showing on skip/incorrect guess

--- src/routes/AlbumArt.svelte
+++ src/routes/AlbumArt.svelte
@@ -1,17 +1,22 @@
 <script lang="ts">
     import "../app.css";
+    import {onMount} from "svelte";
     import type {Album} from "../js/albumpool";
+    import type {GameInstance} from "../js/games.js";
     import {getGameInstance} from "../js/games.js";
 
     export let day: number;
     export let album: Album;
     export let failed: number;
+
+    let game: GameInstance;
+    onMount(() => getGameInstance(day, album).then((game_) => game = game_));
 </script>
 
 <div class="max-w-sm mx-auto flex-grow flex items-center justify-center">
-    {#await getGameInstance(day, album)}
-        Loading...
-    {:then game}
+    {#if game}
         <img src="{game.getCanvasForGuess(failed).toDataURL('image/png')}" alt="Guess That Album!">
-    {/await}
+    {:else}
+        Loading...
+    {/if}
 </div>
