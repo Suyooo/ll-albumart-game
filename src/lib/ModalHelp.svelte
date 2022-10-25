7e7e205b64909a7978359f84314c13a1a6f2bda6
Suyooo <me@suyo.be>
2022-10-25 09:54:27 +0200

    Fix stats window throwing an error before first clear

--- src/lib/ModalHelp.svelte
+++ src/lib/ModalHelp.svelte
@@ -13,21 +13,30 @@
 
 <div class="flex flex-col space-y-4 items-center">
     <div class="flex items-center space-x-4">
-        <div class="text-primary"><Tutorial1/></div>
+        <div class="text-primary">
+            <Tutorial1/>
+        </div>
         <div>
-            You will see a concealed image of an LL! album's cover art. <span class="inline-block">Which one is it?</span>
+            You will see a concealed image of an LL! album's cover art.
+            <span class="inline-block">Which one is it?</span>
         </div>
     </div>
     <div class="flex items-center space-x-4">
-        <div class="text-primary"><Tutorial2/></div>
+        <div class="text-primary">
+            <Tutorial2/>
+        </div>
         <div>
-            If you skip a turn or guess wrong, more parts or details of the picture <span class="inline-block">are revealed!</span>
+            If you skip a turn or guess wrong, more parts or details of the picture
+            <span class="inline-block">are revealed!</span>
         </div>
     </div>
     <div class="flex items-center space-x-4">
-        <div class="text-primary"><Tutorial3/></div>
+        <div class="text-primary">
+            <Tutorial3/>
+        </div>
         <div>
-            Find the correct answer in six turns, and share your result with <span class="inline-block">your friends!</span>
+            Find the correct answer in six turns, and share your result with
+            <span class="inline-block">your friends!</span>
         </div>
     </div>
     <button class="px-3 py-2 rounded p-1 uppercase tracking-widest transition-colors duration-200 bg-primary-500"
