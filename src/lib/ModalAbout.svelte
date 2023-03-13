702dad855bdb0ebd6acb31c2cb4b510029b41656
Suyooo <me@suyo.be>
2023-03-13 16:17:14 +0100

    Add Save Data Import/Export

--- src/lib/ModalAbout.svelte
+++ src/lib/ModalAbout.svelte
@@ -1,8 +1,54 @@
 <script lang="ts">
     // required re-assignment to make vite constants work nice
+    import PageButton from "$lib/styled/PageButton.svelte";
+    import {ALL_STATES} from "$stores/state";
+    import {STATISTICS} from "$stores/statistics";
+    import {getContext} from "svelte";
+
     const CONST_INDEV: boolean = INDEV;
     const CONST_BUILDTIME: number = BUILDTIME;
     const CONST_BUILDDATE: string = BUILDDATE;
+
+    const read = getContext<(s: string, priority?: "polite" | "assertive") => void>("reader");
+    let exported: boolean = false;
+
+    function exportSave() {
+        navigator.clipboard.writeText(JSON.stringify({
+            "states": $ALL_STATES,
+            "statistics": $STATISTICS
+        }))
+            .then(() => {
+                read("Your save data has been copied to your clipboard.", "assertive");
+                exported = true;
+            })
+            .catch((err) => {
+                alert("Unable to share or copy your save data: " + err);
+            });
+    }
+
+    function importSave() {
+        const saveDataEntered = prompt("Please paste your save data here to import it.\n\n" +
+            "WARNING: Any save data you have will be overwritten! (Click Cancel if you don't want that)");
+        if (saveDataEntered !== null) {
+            let saveDataParsed;
+            try {
+                saveDataParsed = JSON.parse(saveDataEntered);
+                console.log(saveDataParsed)
+                if (saveDataParsed["states"]) {
+                    localStorage.setItem("llalbum-states", JSON.stringify(saveDataParsed["states"]));
+                }
+                if (saveDataParsed["statistics"]) {
+                    localStorage.setItem("llalbum-statistics", JSON.stringify(saveDataParsed["statistics"]));
+                }
+                window.location.reload();
+            } catch (e) {
+                alert("There was a problem moving your save data. If the following error contains something like" +
+                    "\"JSON\", make sure you copied and pasted all of the text from the insecure site. If you did, or" +
+                    "there's a different error, please let us know via the links in the About section!\n\n" +
+                    "The error was:\n" + e);
+            }
+        }
+    }
 
     let checkPromise: Promise<string> | null = null;
 
@@ -21,7 +67,13 @@
 
 <div class="flex-col space-y-4">
     <div>
-        This site was made by <a href="https://twitter.com/Suyo_">@Suyo_</a>
+        This site was made by Suyooo
+        <ul class="list-disc list-inside ml-4 text-sm">
+            <li><a href="https://mstdn.schoolidol.club/@Suyooo">Mastodon</a></li>
+            <li><a href="https://www.tumblr.com/suyooo">Tumblr</a></li>
+            <li>Discord: Suyooo#7026</li>
+            <li><a href="https://twitter.com/Suyo_">Twitter</a></li>
+        </ul>
         <div class="text-xs">
             If you have any problems, comments, or ideas for new game modes, feel free to send them to me!
         </div>
@@ -40,6 +92,17 @@
         <a href="https://github.com/Suyooo/ll-albumart-game">Source Code on GitHub</a>
     </div>
     <div>Inspired by Wordle and Heardle</div>
+    <div>
+        <div class="flex gap-x-2">
+            <PageButton class="px-2" label="Export Save Data" on:click={exportSave}>Export Save Data</PageButton>
+            <PageButton class="px-2" label="Import Save Data" on:click={importSave}>Import Save Data</PageButton>
+        </div>
+        {#if exported}
+            <div class="mt-1 text-xs text-gray-400 tracking-tighter leading-4">
+                Your save data has been copied to your clipboard.
+            </div>
+        {/if}
+    </div>
     <div class="mt-6 text-xs text-gray-400 tracking-tighter leading-4">
         Love Live! and all album art, song titles and project/group names are copyrighted: ©PL! ©PL!S ©PL!N ©PL!SP
         ©SUNRISE
