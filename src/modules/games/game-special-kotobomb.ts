c4cc545326c27d84b3addf69b7adb389d5dac84e
Suyooo <me@suyo.be>
2023-03-23 19:38:21 +0100

    Kotobomb mode

--- src/modules/games/game-special-kotobomb.ts
+++ src/modules/games/game-special-kotobomb.ts
@@ -2,8 +2,7 @@
 
 import type {AlbumInfo} from "$data/albumpool";
 import type {Canvas, Image} from "canvas";
-import {createCanvas} from "canvas";
-import {releaseCanvas, smoothScaleSquareWithSrc} from "../canvasUtil";
+import {createCanvas, loadImage} from "canvas";
 import type {GameInstance} from "../gameHandler";
 import {CANVAS_SIZE} from "../gameHandler";
 
@@ -11,21 +10,19 @@ export const stacked = false;
 export const hasAltFinished = false;
 export const forceAltFinished = false;
 
-const SIZES = [5, 9, 15, 20, 30, 55];
+const SIZES = [700, 650, 600, 550, 480, 400];
 
-export function getGameInstance(_day: number, _album: AlbumInfo, _image: Image, scaledImage: Canvas): GameInstance {
+export async function getGameInstance(_day: number, _album: AlbumInfo, _image: Image, scaledImage: Canvas): Promise<GameInstance> {
+    const birbImage = await loadImage("/assets/kotobomb.png");
     const getCanvasForGuess = (failed: number): Canvas => {
-        const tempCanvas = createCanvas(CANVAS_SIZE, CANVAS_SIZE);
         const canvas = createCanvas(CANVAS_SIZE, CANVAS_SIZE);
-        const tempCtx = tempCanvas.getContext("2d");
         const ctx = canvas.getContext("2d");
         const targetSize = SIZES[failed];
 
         ctx.imageSmoothingEnabled = true;
-        smoothScaleSquareWithSrc(tempCtx, scaledImage, 0, 0, CANVAS_SIZE, CANVAS_SIZE, targetSize);
-        ctx.imageSmoothingEnabled = false;
-        ctx.drawImage(tempCanvas, 0, 0, targetSize, targetSize, 0, 0, CANVAS_SIZE, CANVAS_SIZE);
-        releaseCanvas(tempCanvas);
+        ctx.drawImage(scaledImage, 0, 0, CANVAS_SIZE, CANVAS_SIZE, 0, 0, CANVAS_SIZE, CANVAS_SIZE);
+        ctx.drawImage(birbImage, 0, 0, 650, 650, 0, CANVAS_SIZE - targetSize, targetSize, targetSize);
+
         return canvas;
     };
     const getShareCanvas = (): Canvas => {
