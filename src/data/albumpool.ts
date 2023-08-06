a95fc921e78cc17f73849bc8748ca06270a5310b
Suyooo <me@suyo.be>
2023-08-06 23:03:17 +0200

    Remove UNIVERSE from album pool (Not a main album release, according to CD list on official site)

--- src/data/albumpool.ts
+++ src/data/albumpool.ts
@@ -1,15 +1,15 @@
-import json from "./albumpool.json" assert {type: "json"};
+import json from "./albumpool.json" assert { type: "json" };
 
 export interface AlbumInfo {
-    url: string,
-    startOnDay: number,
-    weight: number,
-    titleEn: string,
-    artistEn: string,
-    altEn?: string,
-    titleJa: string,
-    artistJa: string,
-    altJa?: string
+    url: string;
+    startOnDay: number;
+    weight: number | { [weightFromDay: string]: number };
+    titleEn: string;
+    artistEn: string;
+    altEn?: string;
+    titleJa: string;
+    artistJa: string;
+    altJa?: string;
 }
 
 export const ALBUM_POOL: AlbumInfo[] = <AlbumInfo[]>json;
